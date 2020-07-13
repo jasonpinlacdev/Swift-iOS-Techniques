f9f93c7e81603ae4500407035667402b9824abc1
Jason Pinlac <jasonpinlacdev@gmail.com>
2020-07-13 18:44:42 -0400

    UITableView Sections, animation reload rows, and section expand and shrink

---
+++ UITableView With Custom Sections/UITableView With Custom Sections/Models/ExpandableNames.swift
@@ -0,0 +1,15 @@
+//
+//  ExpandableNames.swift
+//  UITableView With Custom Sections
+//
+//  Created by Jason Pinlac on 7/13/20.
+//  Copyright © 2020 Jason Pinlac. All rights reserved.
+//
+
+import UIKit
+
+struct ExpandableNames {
+    var names: [String]
+    var isExpanded: Bool
+}
+
