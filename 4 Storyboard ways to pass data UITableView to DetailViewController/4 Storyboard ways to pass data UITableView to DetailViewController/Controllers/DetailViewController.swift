a29cede615d7bc4a77ca15710f4a5c95e1a13a78
Jason Pinlac <jasonpinlacdev@gmail.com>
2020-10-20 20:32:36 -0400

    added the project that shows 4 different ways of passing data from selected row item on UITableViewCell to the DetailViewController using storyboard segues, storyboard prepare method, storyboard  @IBSegueAction method, programmatically, and programmatically from the storyboard object.

---
+++ 4 Storyboard ways to pass data UITableView to DetailViewController/4 Storyboard ways to pass data UITableView to DetailViewController/Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  4 Storyboard ways to pass data UITableView to DetailViewController
+//
+//  Created by Jason Pinlac on 10/20/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var brother: String?
+
+    @IBOutlet var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabel?.text = brother
+    }
+    
+    required init?(coder: NSCoder, brother: String?) {
+        self.brother = brother
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
---
+++ 4 Storyboard ways to pass data UITableView to DetailViewController/4 Storyboard ways to pass data UITableView to DetailViewController/Controllers/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  4 Storyboard ways to pass data UITableView to DetailViewController
+//
+//  Created by Jason Pinlac on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var data = ["Jason", "Michael", "David"]
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Brothers"
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK: - NEW WAY: passing data to the detailViewController via @IBSegueAction (make sure segue is created from tableView cell to detailController and the outlet is connected) -
+//    @IBSegueAction func showBrotherDetail(_ coder: NSCoder, sender: Any?, segueIdentifier: String?) -> DetailViewController? {
+//        guard let indexPath = tableView.indexPathForSelectedRow else { fatalError("Could not get the indexPath for selected row.")}
+//        let brotherData = data[indexPath.row]
+//        return DetailViewController(coder: coder, brother: brotherData)
+//    }
+    
+    // MARK: - OLD WAY: passing data to the detailViewController via storyboard segue and override prepare method (make sure segue is created from tableView cell to detailController) -
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "BrotherDetailSegue" {
+//            guard let indexPath = tableView.indexPathForSelectedRow else { fatalError("Failed to get indexPath for the selected row.") }
+//            let detailViewController = segue.destination as! DetailViewController
+//            detailViewController.brother = data[indexPath.row]
+//        }
+//    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    //MARK: - OLD WAY: passing data to detailViewController via UITableViewDelegate didSelectRowAt method and instantiation from storyboard id -
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        guard let detailViewController = storyboard?.instantiateViewController(identifier: "DetailViewController") as? DetailViewController else { return }
+//        detailViewController.brother = data[indexPath.row]
+//        navigationController?.pushViewController(detailViewController, animated: true)
+//    }
+    
+    // MARK: - NEW WAY: passing data to the storyboard detailViewController via the use of custom init?(coder: , ...) code -
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let detailViewController = storyboard?.instantiateViewController(identifier: "DetailViewController", creator: { [weak self] coder -> DetailViewController? in
+            let brotherData = self?.data[indexPath.row]
+            return DetailViewController(coder: coder, brother: brotherData)
+        }) else { return }
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return data.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BrotherCell") else { fatalError("Failed to dequeue reusable BrothersCell") }
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+
