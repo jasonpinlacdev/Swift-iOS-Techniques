dd7e49055715c65306c22c5d4b0750da0f41585b
Jason Pinlac <jasonpinlacdev@gmail.com>
2020-10-22 00:52:54 -0400

    Completed the UI for the 5 ways to dismiss the keyboard project

---
+++ 5 Ways Dismiss Keyboard/5 Ways Dismiss Keyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5 Ways Dismiss Keyboard
+//
+//  Created by Jason Pinlac on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
